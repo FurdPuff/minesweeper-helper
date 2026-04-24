44993f015d089a0d92987c675be6194a7c381a83
Nihal M <nihalmir.bc@gmail.com>
2026-04-23 21:19:11 -0400

    Basic Python Minesweeper solver

--- solver/board.py
+++ solver/board.py
@@ -1,6 +1,61 @@
-class board:
-    def __init__(self):
-        self.width
-        self.height
-        self.mineCount
-        self.revealedList
+# board.py initializes Board and Cell classes
+
+from actions import Actions
+
+class Cell:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.revealed = False
+        self.flagged = False
+        self.number: int | None = None
+
+# create_cells(width, height) creates a 2d array of cells
+def create_cells(width: int, height: int) -> list[list[Cell]]:
+    return [[Cell(x, y) 
+             for x in range(width)] 
+             for y in range(height)]
+
+class Board:
+    def __init__(self, width: int, height: int, mineCount: int):
+        self.width = width
+        self.height = height
+        self.mine_count = mineCount
+        self.cells = create_cells(width, height)
+        self.actions = Actions(self)
+    
+    # get_cell(x, y) returns the desired cell in the board given (x,y) coordinates
+    # requires: x and y are within the range of the board
+    @property
+    def get_cell(self, x: int, y: int) -> Cell:
+        if x < 0 or y < 0 or x >= self.width or y >= self.height:
+            raise IndexError('Coordinates out of bounds')
+        return self.cells[y][x]
+    
+    # size() returns size of the board
+    @property
+    def size(self) -> int:
+        return self.width * self.height
+
+    # get_revealed_list() returns a list of revealed cells from a given board
+    # time: O(n), n is size of board
+    @property
+    def revealed_cells(self) -> list[Cell]:
+        list = []
+        for row in self.cells:
+            for cell in row:
+                if cell.revealed: list.append(cell)
+    
+    # insert_cells(cells) inserts a list of cells into the Board
+    # requires: x and y are within the range of the board
+    @property
+    def insert_cells(self, cells: list[Cell]) -> None:
+        for cell in cells:
+            if cell.x < 0 or cell.y < 0 or cell.x >= self.width or cell.y >= self.height:
+                raise IndexError('Coordinates out of bounds')
+            old_cell = self.get_cell(cell.x, cell.y)
+            old_cell.x = cell.x
+            old_cell.y = cell.y
+            old_cell.revealed = cell.revealed
+            old_cell.flagged = cell.flagged
+            old_cell.number = cell.number
--- solver/solve.py
+++ solver/solve.py
@@ -1,15 +1,67 @@
-class Cell:
-    def __init__(self):
-        self.revealed = False
-        self.flagged = False
-        self.number = None #0-8 or None if unrevealed
+# solve.py provides functions that solve a given minesweeper board
 
-Board =list[list[Cell]]
+from board import Board, Cell
 
-# If number == flagged_neighbors then all unrevealed_neighbors = safe
+# apply_basic_solve(board) applies basic minesweeper algorithm to iteratively solve a board, 
+#    returning true if the board has been solved and false otherwise.
+# time: O(n^2) where n is the number of unknown tiles
+def apply_basic_solve(board: Board) -> bool:
+    while (True):
+        revealed_cells: list[Cell] = board.revealed_cells
+        if len(revealed_cells) == total_cells - board.mine_count: break
 
-# If number == unrevealed_neighbors then all unrevealed_neighbors = mine
+        total_cells = board.size
+        has_changed = False
+        
+        # solve each cell
+        for cell in revealed_cells:
+            # counts number of neighboring flags, safe cells, and unknown cells
+            neighbor_safe_count = 0
+            neighbor_flag_count = 0
+            for dy in range(-1, 1):
+                for dx in range(-1, 1):
+                    if dy == 0 and dx == 0: continue
+                    ny = cell.y + dy
+                    nx = cell.x + dx
+                    if ny < 0 or nx < 0 or ny >= board.height or nx >= board.width: continue
 
-def apply_basic_solve(board):
-    for y in (0,3):
-        print
+                    neighbor_cell = board.get_cell(nx, ny)
+                    if neighbor_cell.revealed: neighbor_safe_count += 1
+                    if neighbor_cell.flagged: neighbor_flag_count += 1
+            neighbor_unknown_count = 8 - neighbor_safe_count - neighbor_flag_count
+
+            # solves when cell has adjacent unknown cells
+            if neighbor_unknown_count > 0:
+                # flag unknown neighbors if the number of unknown cells matches the number of
+                #    remaining flags
+                if neighbor_unknown_count + neighbor_flag_count == cell.number: 
+                    for dy in range(-1, 1):
+                        for dx in range(-1, 1):
+                            if dy == 0 and dx == 0: continue
+                            if dy == 0 and dx == 0: continue
+                            ny = cell.y + dy
+                            nx = cell.x + dx
+                            if ny < 0 or nx < 0 or ny >= board.height or nx >= board.width: continue
+                            board.actions.flag(nx, ny)
+                    neighbor_unknown_count = 0
+                    neighbor_flag_count = cell.number
+                    has_changed = True
+                
+                # reveal unknown neighbors if neighboring mines are already flagged
+                elif neighbor_flag_count == cell.number:
+                    for dy in range(-1, 1):
+                        for dx in range(-1, 1):
+                            if dy == 0 and dx == 0: continue
+                            if dy == 0 and dx == 0: continue
+                            ny = cell.y + dy
+                            nx = cell.x + dx
+                            if ny < 0 or nx < 0 or ny >= board.height or nx >= board.width: continue
+                            board.actions.reveal(nx, ny)
+                    neighbor_unknown_count = 0
+                    neighbor_safe_count = 8 - cell.number
+                    has_changed = True
+        
+        # check if board has not been solved by basic algorithm
+        if not has_changed:
+            return False
+    return True
